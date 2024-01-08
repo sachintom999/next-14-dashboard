5501530c7ff4b69ced8eed3cddffc75074ed74cf
Sachin Tom <sachintomacc@gmail.com>
2024-01-08 14:11:53 +0530

    first commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,6 +1,10 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+
+
+
+
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
